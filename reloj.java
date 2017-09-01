2ef37e513183114324823dbb273578c7b396d8da
fernandoFdz <ferykudouxd@hotmail.com>
2017-09-01 03:11:54 -0500

    corregido

--- reloj.java
+++ reloj.java
@@ -2,19 +2,29 @@ public class reloj{
     private int minuto;
     private int hora;
     
-    public void define(){
+    /*
+     * constructor 
+     */
+    public reloj(){
         hora=0;
         minuto=0;
     }
     
+    /*
+     * metodo que muestra la hora
+     */
     public String dimeTuTiempo(){
-        String cadResultado="";
+            String cadResultado="0";
             cadResultado+=hora;
-            cadResultado+=":";
+            cadResultado+=":0";
+            cadResultado+="";
             cadResultado+=minuto;
             return cadResultado;
     }
     
+    /*
+     * metodo que aumenta un minuto cada vez que se utiliza 
+     */
     public void ticToc(){
 
                 if(minuto<60){ 
